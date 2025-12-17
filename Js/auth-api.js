f076c1bfc4bcb5ebeebde4b118bedb022a33075c
Hiago C Felisberto <hiagocf2003@gmail.com>
2025-12-16 22:59:42 -0300

    Api do backend com chave do render atualizada

--- Js/auth-api.js
+++ Js/auth-api.js
@@ -3,7 +3,7 @@
 const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' || window.location.protocol === 'file:';
 const API_URL = isLocalhost
   ? 'http://localhost:3001/api'
-  : `${window.location.protocol}//${window.location.hostname.replace('kprsneakers.vercel.app', 'kprsneakers-api.onrender.com')}/api`;
+  : 'https://kprimports-api.onrender.com/api';
 
 // Função para obter token do localStorage
 function getToken() {
